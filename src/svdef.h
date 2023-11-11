c2005d263539a897d07ad6470c045e1d62380c66
coshcage <58808125+coshcage@users.noreply.github.com>
2023-11-10 19:15:34 -0800

    Add files via upload

--- src/svdef.h
+++ src/svdef.h
@@ -21,7 +21,7 @@
 #include <stddef.h> /* Using type size_t, ptrdiff_t. */
 
 /* Library version. */
-#define LIB_VER "1.2.0.1"
+#define LIB_VER "1.2.0.2"
 
 /* Boolean constants. */
 #define TRUE  1

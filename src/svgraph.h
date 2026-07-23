e082171f6bfde7ed3ac2117e50d7aa9ed3661811
John Cage <58808125+coshcage@users.noreply.github.com>
2026-07-22 23:38:25 -0400

    Add files via upload

--- src/svgraph.h
+++ src/svgraph.h
@@ -2,7 +2,7 @@
  * Name:        svgraph.h
  * Description: Graphs interface.
  * Author:      cosh.cage#hotmail.com
- * File ID:     0901171625S0718260014L00207
+ * File ID:     0901171625S0723260000L00231
  * License:     LGPLv3
  * Copyright (C) 2017-2026 John Cage
  *
@@ -203,5 +203,29 @@ P_GRAPH_M  grpCreateMFromL          (P_GRAPH_L pgrpl);
  *    /         5 \ 7
  *  |/_      6    _\|
  * (Vertex 1)->(Vertex 2)
+ *
+ * Here we explain the addressing issue of an adjacent matrix graph.
+ * Assume we have such an adjacent matrix graph which contains 8 vertices.
+ * The mathematical description of such a matrix is as follows:
+ * G = {V, e} of which V = {a,b,c,d,e,f,g,h}, e(v, v) = w for each v belongs to V.
+ * V is a set of a group of constants, in set V:
+ * a = 0; b = 1; c = 2; d = 3; e = 4; f = 5; g = 6; h = 7;
+ * In memory the graph is set as a matrix of a size_t[8][8] two dimensional array.
+ *    a  b  c  d  e  f  g  h
+ * a  2  3  4  5  6  4  3  2  a
+ * b  1  1  1  1  1  1  1  1  b
+ * c  0  0  0  0  0  0  0  0  c
+ * d  0  0  0  0  0  0  0  0  d
+ * e  0  0  0  0  0  0  0  0  e
+ * f  0  0  0  0  0  0  0  0  f
+ * g  9  9  9  9  9  9  9  9  g
+ * h 10 11 12 13 14 12 11 10  h
+ *    a  b  c  d  e  f  g  h
+ * Notice that line number starts from left up corner to bottom left corner and it stands for the first vertex identifier of an edge.
+ * While column number starts from left up corner to right up corner and it stands for the second vertex identifier of this edge.
+ * Such that edge(x, y) = edge(a, b) = 3. That is edge(x, y) takes x as line, and y as column. The reason why we do this is that
+ * we must coherent to the principle of memory locality while fetching edges in order to alter the weights of theirs.
+ * In case of a cache missing failure to read edge(g, a), the address that begins from edge(g, a) will be pumped in to cache so that
+ * a further read or write of edge(g, y) will be performed by CPU faster.
  */
 
